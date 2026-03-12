ce0ac594fe47cb23e25664b3f55b464540657370
Sinchana Nama <sinchana@thenamas.com>
2026-03-11 20:40:11 -0700

    no play button on mobile trial 5git add .git add .

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ export default function HomePage() {
   return (
     <main className="relative flex h-[100svh] min-h-[100svh] flex-col overflow-hidden text-black dark:text-white">
       <BackgroundCarousel
-        videoSrc="/videos/renao.mp4"
+        videoSrc="/videos/converted_renao.mp4"
         videoSources={[
-          "/videos/renao.mp4",
-          "/videos/zayn.mp4",
-          "/videos/rahul.mp4",
-          "/videos/hasan.mp4",  
+          "/videos/converted_renao.mp4",
+          "/videos/converted_zayn.mp4",
+          "/videos/converted_rahul.mp4",
+          "/videos/converted_hasan.mp4",  
         ]}
       />
 
